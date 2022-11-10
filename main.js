d08e54e76547e57a202c5e16ee79659131e1e639
shubhadats <74710929+shubhadats@users.noreply.github.com>
2022-11-10 18:07:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -18,7 +18,26 @@ var content = event.results[0][0].transcript;
 
     document.getElementById("textbox").innerHTML = content;
     console.log(content);
+    speak();
    
 }
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data = "Taking your Selfie in 5 seconds";
+       
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    
+    synth.speak(utterThis);
+
+    Webcam.attach(camera);
+}
+camera = document.getElementById("camera");
+Webcam.set({
+    width:360,
+    height:250,
+    image_format : 'png',
+    png_quality:90
+});
 
 
